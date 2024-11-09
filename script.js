af98bd79128266b6d0a4d4da114793372619e9c4
Wellingtonalves33 <wellingtonvdtc@gmail.com>
2024-11-09 20:09:29 -0300

    jodo do mario

---
+++ script.js
@@ -0,0 +1,39 @@
+
+const pipe  = document.querySelector(".pipe")
+const mario = document.querySelector(".mario")
+const restart = document.querySelector(".restart")
+
+const jump = () =>{
+mario.classList.add("jump")
+setTimeout(() => {
+    mario.classList.remove("jump")
+},500); 
+}
+
+const loop = setInterval(()=> {
+const pipePosition = pipe.offsetLeft;
+const marioPosition = +window.getComputedStyle(mario).bottom.replace("px","")
+
+
+if (pipePosition <= 100 && pipePosition > 0 && marioPosition < 80 ){
+    pipe.style.animation = "nome"
+    pipe.style.left = `${pipePosition}px`
+
+   mario.style.animation = "nome"
+    mario.style.bottom = `${marioPosition}px`
+
+
+    mario.src = "./assets/game-over.png"
+    mario.style.width = "60px"
+    mario.style.marginLeft = "23px"
+    clearInterval(loop)
+}
+
+},10)
+
+
+
+restart.addEventListener("click", () => {
+    location.reload(true)
+})
+document.addEventListener("keydown", jump)
